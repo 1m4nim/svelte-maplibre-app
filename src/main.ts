0182aac6351a48bcba458b24eec30eeabdedb118
1m4nim <fancustard9@gmail.com>
2025-06-27 08:19:51 +0900

    main.tsにimportされていないものがあった

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import "maplibre-gl/dist/maplibre-gl.css"
 
 const app = mount(App, {
   target: document.getElementById('app')!,
